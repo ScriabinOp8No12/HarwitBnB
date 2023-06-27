c96de1ba050b1313dbd728cfff1e7aad160eff16
nharwit <nharwit@gmail.com>
2023-06-26 21:09:06 -0600

    everything through phase 2 SHOULD be working!

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,19 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+// Test this in the browser at: http://localhost:8000/api/csrf/restore
+// fetch("/api/test", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `44MyvdFw-jZf5SmTaxsyQE-1ooBKScXruaPE`,
+//   },
+//   body: JSON.stringify({ hello: "world" }),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
