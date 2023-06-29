298771abb896f65ed66029e0cad443b93388436a
nharwit <nharwit@gmail.com>
2023-06-28 22:14:05 -0600

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -42,35 +42,10 @@ router.post("/", async (req, res, next) => {
   });
 });
 
+// Log out
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
+});
+
 module.exports = router;
-
-// fetch request in browser's console (first 2 work, last one has wrong password so it fails!):
-
-// fetch("/api/session", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "XSRF-TOKEN": `pwQR4f6l-nrjuE2_biwLF1Bhb6DB_DYQ-H54`,
-//   },
-//   body: JSON.stringify({ credential: "Demo-lition", password: "password" }),
-// })
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
-
-// fetch('/api/session', {
-//   method: 'POST',
-//   headers: {
-//     "Content-Type": "application/json",
-//     "XSRF-TOKEN": `pwQR4f6l-nrjuE2_biwLF1Bhb6DB_DYQ-H54`
-//   },
-//   body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
-// }).then(res => res.json()).then(data => console.log(data));
-
-// fetch('/api/session', {
-//   method: 'POST',
-//   headers: {
-//     "Content-Type": "application/json",
-//     "XSRF-TOKEN": `pwQR4f6l-nrjuE2_biwLF1Bhb6DB_DYQ-H54`
-//   },
-//   body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
-// }).then(res => res.json()).then(data => console.log(data));
