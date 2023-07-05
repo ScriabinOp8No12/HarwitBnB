757086ea4474774c19140e83beb037c14bf379e1
nharwit <nharwit@gmail.com>
2023-07-05 00:04:36 -0600

    renamed 04-demo-review to have one hyphen at the start, and added seed data for review image, I used reviewId of 1, 2, and 3 because we have 3 reviews from the review seed data

--- backend/db/seeders/06-demo-review-image.js
+++ backend/db/seeders/06-demo-review-image.js
@@ -1,25 +1,43 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    options.tableName = "ReviewImages";
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+          reviewId: 1,
+          url: "https://example.com/image7.jpg",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          reviewId: 2,
+          url: "https://example.com/image8.jpg",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          reviewId: 3,
+          url: "https://example.com/image9.jpg",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    options.tableName = "ReviewImages";
+    await queryInterface.bulkDelete(options, null, {});
+  },
 };
