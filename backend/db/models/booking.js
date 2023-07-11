c989fe018bc6a16d5063f8d117cf2649148e11ac
nharwit <nharwit@gmail.com>
2023-07-10 23:35:02 -0600

    removed delete on cascade from bookings

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      // removed delete on cascade from below two associations!
       Booking.belongsTo(models.Spot, {
         foreignKey: "spotId",
       });
