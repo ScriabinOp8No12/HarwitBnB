2ebc7f38bf56e83c13fbb8c856e26c442891cf31
nharwit <nharwit@gmail.com>
2023-08-23 13:07:23 -0600

    added store, component and css for spot details, most of the logic is there, but the fetch request isn't working yet

---
+++ frontend/src/components/SpotDetail.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchSpot } from "../store/spotDetail"; // import fetchSpot (singular) thunk
+import "./styles/SpotDetail.css";
+
+function SpotDetail() {
+  const dispatch = useDispatch();
+  const { id } = useParams(); // Get spot ID from the URL
+  const spot = useSelector((state) => state.spot.spot); // Getting specific spot from store
+
+  // Fetching specific spot when component mounts
+  useEffect(() => {
+    dispatch(fetchSpot(id));
+  }, [dispatch, id]);
+
+  // handling reservation click, need to sometimes ghost it out / disable it like the login in button
+  const handleReservationClick = () => {
+    alert("Feature coming soon");
+  };
+
+  // seleting main image and 4 other images, which will be styled to be smaller with css
+  // ? is optional chaining, it won't throw an error if SpotImages doesn't exist, it'll just returned undefined instead
+  const mainImage = spot.SpotImages?.find((image) => image.preview)?.url;
+  const otherImages = spot.SpotImages?.filter((image) => !image.preview).slice(
+    0,
+    4
+  );
+
+  return (
+    <div className="spotDetailContainer">
+      <h1>{spot.name}</h1>
+      <div className="location">
+        {spot.city}, {spot.state}, {spot.country}
+      </div>
+      <div className="images">
+        <img src={mainImage} alt={spot.name} className="largeImage" />
+        <div className="smallImages">
+          {otherImages.map((image, index) => (
+            <img
+              src={image.url}
+              alt={`${spot.name} ${index}`}
+              key={index}
+              className="smallImage"
+            />
+          ))}
+        </div>
+      </div>
+      <div className="priceReviewBox">
+        <div className="price">
+          ${spot.price} <span>night</span>
+        </div>
+        <button onClick={handleReservationClick} className="reserveButton">
+          Reserve
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SpotDetail;
