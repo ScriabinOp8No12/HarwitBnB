ed924d0d7b9672f7b31200922d7a1b242e4e3890
nharwit <nharwit@gmail.com>
2023-08-24 19:58:03 -0600

    most of form template done, need to implement and test logic / errors, and do CSS

--- frontend/src/components/CreateSpotForm.js
+++ frontend/src/components/CreateSpotForm.js
@@ -1,52 +1,250 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { createSpot } from "../store/spots";
 
 function CreateSpotForm() {
+  const dispatch = useDispatch();
+  const history = useHistory();
   const [spotDetails, setSpotDetails] = useState({
-    name: "",
-    location: "",
-    // Add other fields as needed
+    // All the form input fields
+    country: "",
+    streetAddress: "",
+    city: "",
+    state: "",
+    description: "",
+    latitude: "",
+    longitude: "",
+    title: "",
+    price: "",
+    previewImage: "",
+    images: ["", "", "", ""],
   });
+  const [imageError, setImageError] = useState(false);
+  // Handle errors as an array
+  const [errors, setErrors] = useState([]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setSpotDetails((prevDetails) => ({
-      ...prevDetails,
-      [name]: value,
-    }));
+    if (name.startsWith("image")) {
+      const index = Number(name.split("-")[1]);
+      spotDetails.images[index] = value;
+      setSpotDetails({ ...spotDetails });
+    } else {
+      setSpotDetails((prevDetails) => ({
+        ...prevDetails,
+        [name]: value,
+      }));
+    }
+  };
+
+  const validateForm = () => {
+    const newErrors = [];
+    for (const [key, value] of Object.entries(spotDetails)) {
+      if (!value && key !== "images") {
+        newErrors.push(
+          `${key.charAt(0).toUpperCase() + key.slice(1)} is required`
+        );
+      }
+    }
+    if (spotDetails.description.length < 30) {
+      newErrors.push("Description needs 30 or more characters");
+    }
+    if (!spotDetails.previewImage) {
+      newErrors.push("Preview Image URL is required");
+    }
+    return newErrors;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Logic to create a new spot goes here
+    // Handle image extension errors on form submission
+    const imageFields = [
+      spotDetails.previewImage,
+      spotDetails.image1,
+      spotDetails.image2,
+      spotDetails.image3,
+      spotDetails.image4,
+    ];
+    // Regex for ending with .jpg, .jpeg, and .png
+    const invalidImage = imageFields.some(
+      (url) => url && !url.match(/\.(jpg|jpeg|png)$/)
+    );
+
+    if (invalidImage) {
+      setImageError(true);
+      return;
+    }
+
+    const validationErrors = validateForm();
+    if (validationErrors.length === 0) {
+      dispatch(createSpot(spotDetails)).then((newSpot) => {
+        history.push(`/spots/${newSpot.id}`); // Redirect to the new spot's detail page
+      });
+    } else {
+      setErrors(validationErrors);
+    }
   };
 
   return (
     <div>
       <h1>Create a New Spot</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Name:
+        <section>
+          <h2>Where's your place located?</h2>
+          <p>
+            Guests will only get your exact address once they booked a
+            reservation.
+          </p>
+          <label>
+            Country:
+            <input
+              type="text"
+              name="country"
+              placeholder="Country"
+              value={spotDetails.country}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Street Address:
+            <input
+              type="text"
+              name="address"
+              placeholder="Street Address"
+              value={spotDetails.address}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            City:
+            <input
+              type="text"
+              name="city"
+              placeholder="City"
+              value={spotDetails.city}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            State:
+            <input
+              type="text"
+              name="state"
+              placeholder="State"
+              value={spotDetails.state}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Latitude:
+            <input
+              type="text"
+              name="latitude"
+              placeholder="Latitude"
+              value={spotDetails.latitude}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Longitude:
+            <input
+              type="text"
+              name="longitude"
+              placeholder="Longitude"
+              value={spotDetails.longitude}
+              onChange={handleChange}
+            />
+          </label>
+        </section>
+        <section>
+          <h2>Describe your place to guests</h2>
+          <p>
+            Mention the best features of your space, any special amenities like
+            fast wifi or parking, and what you love about the neighborhood.
+          </p>
+          <textarea
+            name="description"
+            placeholder="Please write at least 30 characters"
+            value={spotDetails.description}
+            onChange={handleChange}
+          ></textarea>
+        </section>
+        <section>
+          <h2>Create a title for your spot</h2>
+          <p>
+            Catch guests' attention with a spot title that highlights what makes
+            your place special.
+          </p>
           <input
             type="text"
-            name="name"
-            value={spotDetails.name}
+            name="title"
+            placeholder="Name of your spot"
+            value={spotDetails.title}
             onChange={handleChange}
           />
-        </label>
-        <label>
-          Location:
+        </section>
+        <section>
+          <h2>Set a base price for your spot</h2>
+          <p>
+            Competitive pricing can help your listing stand out and rank higher
+            in search results.
+          </p>
+          <input
+            type="number"
+            name="price"
+            placeholder="Price per night (USD)"
+            value={spotDetails.price}
+            onChange={handleChange}
+          />
+        </section>
+        <section>
+          <h2>Liven up your spot with photos</h2>
+          <p>Submit a link to at least one photo to publish your spot.</p>
           <input
             type="text"
-            name="location"
-            value={spotDetails.location}
+            name="previewImage"
+            placeholder="Preview Image URL"
+            value={spotDetails.previewImage}
             onChange={handleChange}
           />
-        </label>
-        {/* Add other input fields as needed */}
+          <input
+            type="text"
+            name="image1"
+            placeholder="Image URL"
+            value={spotDetails.image1}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="image2"
+            placeholder="Image URL"
+            value={spotDetails.image2}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="image3"
+            placeholder="Image URL"
+            value={spotDetails.image3}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="image4"
+            placeholder="Image URL"
+            value={spotDetails.image4}
+            onChange={handleChange}
+          />
+          {imageError && (
+            <p className="errorMessage">
+              Image URL needs to end in png, jpg, or jpeg.
+            </p>
+          )}
+        </section>
         <button type="submit">Create Spot</button>
       </form>
     </div>
   );
 }
-
 export default CreateSpotForm;
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,18 +1,42 @@
 // Action type
 const SET_SPOTS = "spots/SET_SPOTS";
+// Create spot form action type
+const CREATE_SPOT = "spots/CREATE_SPOT";
 
-// Action creators
+// Action create to set the spot for home page
 export const setSpots = (spots) => ({
   type: SET_SPOTS,
   spots,
 });
 
+// Action creator for creating spot
+export const createSpotAction = (spot) => ({
+  type: CREATE_SPOT,
+  spot,
+});
+
 // Thunks
+// Fetch spots for home page
 export const fetchSpots = () => async (dispatch) => {
   // get the response from the get all spots endpoint, which is at /api/spots
   const response = await fetch("/api/spots");
   const { Spots } = await response.json(); // destructure Spots from response
   dispatch(setSpots(Spots)); // pass Spots array to the action creator
+};
+
+// Create spot form thunk
+
+export const createSpot = (spotDetails) => async (dispatch) => {
+  const response = await fetch("/api/spots", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(spotDetails),
+  });
+  const newSpot = await response.json();
+  dispatch(createSpotAction(newSpot));
+  return newSpot;
 };
 
 // Reducer
@@ -22,6 +46,8 @@ export default function spotsReducer(state = initialState, action) {
   switch (action.type) {
     case SET_SPOTS:
       return { ...state, spots: action.spots };
+    case CREATE_SPOT:
+      return { ...state, spots: [...state.spots, action.spot] };
     default:
       return state;
   }
