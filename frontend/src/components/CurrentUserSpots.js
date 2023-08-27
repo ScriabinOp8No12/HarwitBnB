5d6cb2d94070924803bc80c666a19df61d543ec6
nharwit <nharwit@gmail.com>
2023-08-26 18:10:38 -0600

    added h1 to top of current spots' images and added button below that, need to render it based on conditions now

--- frontend/src/components/CurrentUserSpots.js
+++ frontend/src/components/CurrentUserSpots.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchCurrentUserSpots } from "../store/spots";
-// import "./styles/ManageSpots.css";
+import "./styles/ManageSpots.css";
 
 // Function to format the star rating / round it properly
 function formatRating(rating) {
@@ -23,50 +23,55 @@ function CurrentUserSpots() {
   }, [dispatch]);
 
   return (
-    <div className="containerDiv">
-      {spots &&
-        spots.map((spot) => {
-          // Call formatRating for each individual spot
-          const formattedRating = formatRating(spot.avgRating);
+    <>
+      <h1 className="manageSpots">Manage Spots</h1>
+      <button className="createNewSpotButton">Create a New Spot</button>
 
-          return (
-            // Linking to detail page for each spot
-            <Link to={`/spots/${spot.id}`} key={spot.id} className="spotLink">
-              {/* Tooltip with name of the spot */}
-              <div title={spot.name} className="spotContainer">
-                {/* Display image if it exists */}
-                <div className="previewImage">
-                  {spot.previewImage && (
-                    <img src={spot.previewImage} alt={spot.name} />
-                  )}
-                </div>
-                <div className="textStarsContainer">
-                  <div className="spotDetails">
-                    {/* Displaying city and state of the spot */}
-                    <span>
-                      {spot.city}, {spot.state}
-                    </span>
-                    <div>
-                      {/* Display price of spot */}
-                      <span className="price">${spot.price}</span>{" "}
-                      <span>night</span>
-                    </div>
-                  </div>
-                  <div className="stars">
-                    {/* Displaying average star rating if it exists, otherwise displays "New" */}
-                    {spot.avgRating ? (
-                      <span>★ {formattedRating}</span>
-                    ) : (
-                      <span>New</span>
+      <div className="containerDiv">
+        {spots &&
+          spots.map((spot) => {
+            // Call formatRating for each individual spot
+            const formattedRating = formatRating(spot.avgRating);
+
+            return (
+              // Linking to detail page for each spot
+              <Link to={`/spots/${spot.id}`} key={spot.id} className="spotLink">
+                {/* Tooltip with name of the spot */}
+                <div title={spot.name} className="spotContainer">
+                  {/* Display image if it exists */}
+                  <div className="previewImage">
+                    {spot.previewImage && (
+                      <img src={spot.previewImage} alt={spot.name} />
                     )}
                   </div>
-                  {/* textStarsContainer wraps all the text up to here */}
+                  <div className="textStarsContainer">
+                    <div className="spotDetails">
+                      {/* Displaying city and state of the spot */}
+                      <span>
+                        {spot.city}, {spot.state}
+                      </span>
+                      <div>
+                        {/* Display price of spot */}
+                        <span className="price">${spot.price}</span>{" "}
+                        <span>night</span>
+                      </div>
+                    </div>
+                    <div className="stars">
+                      {/* Displaying average star rating if it exists, otherwise displays "New" */}
+                      {spot.avgRating ? (
+                        <span>★ {formattedRating}</span>
+                      ) : (
+                        <span>New</span>
+                      )}
+                    </div>
+                    {/* textStarsContainer wraps all the text up to here */}
+                  </div>
                 </div>
-              </div>
-            </Link>
-          );
-        })}
-    </div>
+              </Link>
+            );
+          })}
+      </div>
+    </>
   );
 }
 
