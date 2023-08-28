a013989e1f4bbbaafdd83b06e1dfe8b5097fa9d0
nharwit <nharwit@gmail.com>
2023-08-28 17:38:37 -0600

    create spot form fixed up now

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -66,7 +66,7 @@ module.exports = (sequelize, DataTypes) => {
               !Validator.matches(value, /^(?=.*[a-zA-Z0-9])[a-zA-Z0-9\s]*$/)
             ) {
               throw new Error(
-                "Address must contain a combination of letters and numbers. White spaces are allowed too!"
+                "Invalid Address, it must contain a combination of letters and numbers, white spaces are allowed."
               );
             }
           },
